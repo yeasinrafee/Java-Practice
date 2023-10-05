8d9232ba0474e04f8a58dc6c5e2da76b6084d277
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-10-05 21:17:29 +0600

    Palindrome Pyramid

--- Advance Pattern/ButterflyPattern.java
+++ Advance Pattern/ButterflyPattern.java
@@ -2,9 +2,6 @@
 public class ButterflyPattern {
     public static void main(String[] args){
         int n = 4;
-
-
-        //upper part
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
@@ -22,21 +19,14 @@ public class ButterflyPattern {
             }
             System.out.println();
         }
- 
-
-       //lower part
        for(int i=n; i>=1; i--) {
         for(int j=1; j<=i; j++) {
             System.out.print("*");
         }
-
-
         int spaces = 2 * (n-i);
         for(int j=1; j<=spaces; j++) {
             System.out.print(" ");
         }
-
-
         for(int j=1; j<=i; j++) {
             System.out.print("*");
         }
