287ba80eae511f77c3b093eba9ac020c3f76cc91
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-10-07 21:08:22 +0600

    Simple arrya

---
+++ Data Structures/Array/Array.java
@@ -0,0 +1,13 @@
+public class Array{
+    public static void main(String[] args){
+        int[] arr = new int[4];
+        arr[0] = 6;
+        arr[1] = 7;
+        arr[2] = 8;
+        arr[3] = 9;
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
