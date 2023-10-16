0485a3244ebf0130072a1c9a00c52532fe26fa3d
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-10-16 21:49:01 +0600

    Order of Three Cities

--- Book Problems/Chapter 4/OrderThreeCityies.java
+++ Book Problems/Chapter 4/OrderThreeCityies.java
@@ -2,14 +2,40 @@ import java.util.*;
 
 public class OrderThreeCityies {
     public static void main(String[] args){
-        String[] cityNames = new String[3];
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first city: ");
-        cityNames[0] = sc.nextLine();
-        cityNames[1] = sc.nextLine();
-        cityNames[2] = sc.nextLine();
-        String[] newCityNames = new String[3];
-        newCityNames = Arrays.sort(cityNames);
+
+
+        System.out.print("Enter the first country: ");
+        String country1 = sc.nextLine();
+        System.out.print("Enter the second country: ");
+        String country2 = sc.nextLine();
+        System.out.print("Enter the third country: ");
+        String country3 = sc.nextLine();
+
+        String temp;
+        if ((country2.compareTo(country1) > 0) && (country2.compareTo(country3) > 0))
+        {
+            temp = country1;
+            country1 = country2;
+            country2 = temp;
+        }
+        else if ((country3.compareTo(country1) > 0) && (country3.compareTo(country2) > 0))
+        {
+            temp = country1;
+            country1 = country3;
+            country3 = temp;
+
+        }
+
+        if (country3.compareTo(country2) > 0)
+        {
+            temp = country2;
+            country2 = country3;
+            country3 = temp;
+        }
+
+        System.out.println("The three cities in alphabetical order are " +
+                country1 + " " + country2 + " " + country3);
         
     }
 }
