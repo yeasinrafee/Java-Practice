ca7f6bde911de147a75d1951f55d51bdc0fde39a
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-10-17 22:08:20 +0600

    Insertion sort

---
+++ Algorithms/Sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+public class InsertionSort {
+    public static void printArray(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int arr[] = {4, 3, 8, 6, 6, 7, 1};
+
+        for(int i = 1; i < arr.length; i++){
+            int curr = arr[i];
+            int j = i - 1;
+            while(j >= 0 && curr < arr[j]){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = curr;
+        }
+        printArray(arr);
+    }
+}
