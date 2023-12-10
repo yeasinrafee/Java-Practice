581597944264627ff3f0e571ccb0575834d7bed2
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-12-10 21:32:20 +0600

    More string methods

--- src/StringMethods.java
+++ src/StringMethods.java
@@ -1,16 +1,21 @@
 public class StringMethods {
     public static void main(String args[]){
-        String name = " Rafee ";
+        String name = "Rafee";
 
-//        boolean result = name.equalsIgnoreCase("rafee");
 //        int result = name.length();
-//        char result = name.charAt(3);
-//        int result = name.indexOf("e");
-//        boolean result = name.isEmpty();
-//        String result = name.toUpperCase();
 //        String result = name.toLowerCase();
+//        String result = name.toUpperCase();
 //        String result = name.trim();
-        String result = name.replace("R", "N");
+//        String result = name.substring(2, 4);
+//        String result = name.replace("R", "N");
+//        boolean result = name.startsWith("Ra");
+//        boolean result = name.endsWith("ra");
+//        char result = name.charAt(3);
+//        int result = name.indexOf("e", 4);
+//        boolean result = name.equals("rafee");
+//        boolean result = name.equalsIgnoreCase("raFee");
+        boolean result = name.isEmpty();
+
         System.out.println(result);
     }
 }
---
+++ src/Strings.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class Strings {
+    public static void main(String[] args) {
+//        int a = 56;
+//        float b = 35.343f;
+//        String st = "Rafee";
+//        System.out.printf("The value of a is: %d and b is: %9.2f \n", a, b);
+//        System.out.println(st);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
